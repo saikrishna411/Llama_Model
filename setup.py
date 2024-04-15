0dd296ad0a8f5204e027cda01cb87b58b924f33b
saikrishna411 <korupoju411@gmail.com>
2024-04-15 23:04:34 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'AI Repo',
+    version= '0.0.0',
+    author= 'saikrishna',
+    author_email= 'boktiar@ineuron.ai',
+    packages= find_packages(),
+    install_requires = []
+
+)
